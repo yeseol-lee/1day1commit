368b8889519f633414298526c7fd978240ae9434
yeseol-lee <1999onell@gmail.com>
2022-01-04 20:39:33 +0900

    eloquent javascript chapter3 example

---
+++ 220104_eloquent_javascript/minimum.js
@@ -0,0 +1,9 @@
+function minimum(arg1, arg2) {
+  return arg1 < arg2 ? arg1 : arg2;
+}
+
+console.log(minimum(1, 2));
+
+console.log(minimum(1, -2));
+
+console.log(minimum(1.78, 2));
---
+++ 220104_eloquent_javascript/recursion.js
@@ -0,0 +1,19 @@
+function isEven(num) {
+  if (num < 0) {
+    console.log("bad request");
+    return;
+  }
+  if (num === 0) {
+    return true;
+  } else if (num === 1) {
+    return false;
+  } else {
+    return isEven(num - 2);
+  }
+}
+
+console.log(isEven(50));
+
+console.log(isEven(75));
+
+console.log(isEven(-1));
